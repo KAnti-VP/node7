c055151ccec81b1cad3c55b17cc440d8d61bd87e
KAnti-VP <144044786+KAnti-VP@users.noreply.github.com>
2024-10-21 08:35:41 +0200

    modified admin getEditProduct renders views path

--- controllers/admin.js
+++ controllers/admin.js
@@ -28,7 +28,7 @@ export const getEditProduct = (req, res, next) => {
   if (!product) {
     return res.redirect('/')
   }
-  res.render('/admin/edit-product', {
+  res.render('admin/edit-product', {
     pageTitle: 'Edit Product',
     path: '/admin/edit-product',
     editing: editMode,
